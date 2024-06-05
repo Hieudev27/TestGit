306c3fbd0009aee820953356ae8ecc6eb5315988
sonln <sonln@apero.vn>
2024-06-05 11:22:20 +0700

    change phone number

--- app/src/main/java/com/example/dangtrunghuy/User.kt
+++ app/src/main/java/com/example/dangtrunghuy/User.kt
@@ -1,9 +1,9 @@
 package com.example.dangtrunghuy
 
 data class User(
-    var userName:String,
-    var email:String,
-    var phone:String,
-    var password:String,
-    var country:String
+    var userName: String,
+    var email: String,
+    var phoneNumber: String,
+    var password: String,
+    var country: String
 )
